10a93ffa2adc63c9c465e7ec18589ade2acb8588
Ashes.Sarin <Ashes.Sarin@gmail.com>
2013-04-26 12:38:46 +0000

    Added @Transactional on the method

--- chapter 6/ch06-springbankapp-hibernate/src/main/java/sample/spring/chapter06/springbankapp/service/BankAccountServiceImpl.java
+++ chapter 6/ch06-springbankapp-hibernate/src/main/java/sample/spring/chapter06/springbankapp/service/BankAccountServiceImpl.java
@@ -2,6 +2,7 @@ package sample.spring.chapter06.springbankapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import sample.spring.chapter06.springbankapp.dao.BankAccountDao;
 import sample.spring.chapter06.springbankapp.domain.BankAccountDetails;
@@ -13,6 +14,7 @@ public class BankAccountServiceImpl implements BankAccountService {
 	private BankAccountDao bankAccountDao;
 
 	@Override
+	@Transactional
 	public int createBankAccount(BankAccountDetails bankAccountDetails) {
 		return bankAccountDao.createBankAccount(bankAccountDetails);
 	}
